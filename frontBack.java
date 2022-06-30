29d46b2aa490bab9d7a7fdaf516743e438391948
David Pinto <PintoDMA@gmail.com>
2022-06-29 23:06:43 -0400

    Create frontBack.java

---
+++ frontBack.java
@@ -0,0 +1,16 @@
+// Given a string, return a new string where the first and last chars have been exchanged.
+
+
+// frontBack("code") → "eodc"
+// frontBack("a") → "a"
+// frontBack("ab") → "ba"
+// Go...Save, Co
+
+public String frontBack(String str) {
+  if(str.length() <= 1){
+    return str;
+  }
+  
+  String mid = str.substring(1, str.length() - 1);
+  return str.charAt(str.length() - 1) + mid + str.charAt(0);
+}
